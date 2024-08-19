253ede3e00f87d471e9037c662d7488e11fce672
Caldas1003 <159201374+Caldas1003@users.noreply.github.com>
2024-08-19 01:30:59 -0300

    Update funcao_custo.py

--- funcao_custo.py
+++ funcao_custo.py
@@ -2,8 +2,7 @@ import numpy as np
 from massa_mola_amortecedor import MassaMolaAmortecedor
 from entrada_pista import entrada, trepidacao_pista
 
-
-def funcao_custo(params, m=200, x0=[0, 10], track=None):
+def funcao_custo(params, m=250, x0=[0, 0], track=None):
     k, c = params
     sistema = MassaMolaAmortecedor(m, k, c)
 
@@ -26,9 +25,21 @@ def funcao_custo(params, m=200, x0=[0, 10], track=None):
     position_std = np.std(positions)
 
     max_position = np.max(np.abs(positions))
-    penalty = max_position ** 4  # Penalidade para grandes oscilações
+    penalty_position = max_position ** 4
 
-    # Ajuste o fator de penalização conforme necessário
-    cost = position_std * penalty
+    damping_ratio = c / (2 * np.sqrt(k * m))
+
+    # Penalidade se ξ estiver fora do intervalo [0, 1] (subamortecido)
+    penalty_damping_ratio = 0
+    if damping_ratio <= 0 or damping_ratio >= 1:
+        penalty_damping_ratio = 1e6  
+
+    # Penalidade adicional se ξ estiver muito baixo (muito próximo de 0)
+    penalty_low_damping = 0
+    if damping_ratio < 0.1:  
+        penalty_low_damping = 1e3  # Penalidade moderada para valores muito baixos
+
+    cost = position_std * penalty_position + penalty_damping_ratio + penalty_low_damping
 
     return cost
+

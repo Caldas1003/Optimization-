1fa8557769de7f97a37efa459ef943885adef04a
Caldas1003 <159201374+Caldas1003@users.noreply.github.com>
2024-12-03 22:29:24 -0300

    Update Integrador pista - carro.py

--- Integrador pista - carro.py
+++ Integrador pista - carro.py
@@ -10,7 +10,7 @@ class SimulacaoCarro:
         self.tracado = []
         self.angulos = []
 
-    def gerar_checkpoints(self, num_checkpoints=49, num_portoes=10000):
+    def gerar_checkpoints(self, num_checkpoints=49, num_portoes=10):
         t = np.linspace(0, len(self.pista.X_centro) - 1, num_checkpoints, dtype=int)
         for i in t:
             x_esq, y_esq = self.pista.esquerda[i]
@@ -18,17 +18,18 @@ class SimulacaoCarro:
             checkpoint = {'esquerda': (x_esq, y_esq), 'direita': (x_dir, y_dir)}
             self.checkpoints.append(checkpoint)
 
-            # Criar portões igualmente espaçados
+            # Criar portões igualmente espaçados com margem de segurança
+            margem = 0.15  # Margem de segurança como porcentagem da largura
             portoes_checkpoint = []
             for j in range(num_portoes):
-                alfa = j / (num_portoes - 1)
+                alfa = margem + j * (1 - 2 * margem) / (num_portoes - 1)
                 x = x_esq + alfa * (x_dir - x_esq)
                 y = y_esq + alfa * (y_dir - y_esq)
                 portoes_checkpoint.append((x, y))
             self.portoes.append(portoes_checkpoint)
 
-    def simular_tracado(self, angulo_maximo=90):
-        portao_atual = self.portoes[0][0]  # Começa no primeiro portão
+    def simular_tracado(self, angulo_maximo=10):
+        portao_atual = self.portoes[0][len(self.portoes[0]) // 2]  # Começa no portão central
         self.tracado.append(portao_atual)
 
         for i in range(len(self.portoes) - 1):
@@ -82,9 +83,9 @@ class SimulacaoCarro:
             angulo = np.degrees(np.arccos(cos_theta))
             angulos.append(angulo)
 
-        # if not angulos:
-        #     return 0, 0, 0  # Retornar valores padrão
-        print(angulos)
+        if not angulos:
+            return 0, 0, 0  # Retornar valores padrão
+
         max_ang = max(angulos)
         min_ang = min(angulos)
         mean_ang = sum(angulos) / len(angulos)
@@ -115,7 +116,7 @@ class SimulacaoCarro:
 
 
 # Instanciar a pista e a simulação
-pista = Pista(gps_coords, largura_pista=10)
+pista = Pista(gps_coords, largura_pista=6)
 simulacao = SimulacaoCarro(pista)
 
 # Executar a simulação

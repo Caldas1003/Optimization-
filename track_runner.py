580eaf4285ee2c3b4a04883ef3147d3e9d1f27e6
Nikolas Rueckmann <nkls99@gmail.com>
2024-12-12 21:37:38 -0300

    adiciona função para calcular tempo de volta e raio de curva

--- track_runner.py
+++ track_runner.py
@@ -17,6 +17,25 @@ def calculate_angle(P1, P2, P3):
     return np.degrees(angle_in_rad)
 
 
+def get_turn_radius(P1, P2, P3):
+    p2_p3_len = np.sqrt((P3 - P2)**2)
+    p1_p3_len = np.sqrt((P3 - P1)**2)
+    p1_p2_len = np.sqrt((P2 - P1)**2)
+
+    stretch_1 = P2 - P1
+    stretch_2 = P3 - P2
+    vector_product = (
+        stretch_1[1] * stretch_2[2] - stretch_1[2] * stretch_2[1],
+        stretch_1[2] * stretch_2[0] - stretch_1[0] * stretch_2[2],
+        stretch_1[0] * stretch_2[1] - stretch_1[1] * stretch_2[0]
+    )
+    area = np.sqrt(vector_product[0]**2 + vector_product[1]**2 + vector_product[2]**2) / 2
+
+    if area == 0:
+        return 0
+     
+    return (p2_p3_len * p1_p3_len * p1_p2_len) / (4 * area)
+
 
 def total_distance(waypoints: list) -> float:
     total_distance = 0
@@ -30,6 +49,49 @@ def total_distance(waypoints: list) -> float:
         total_distance += distance_between_waypoints
 
     return total_distance
+
+def lap_time(lap: list) -> tuple:
+    g = 9.8 # gravity (9.8 m/s²)
+    allowed_centrifugal_force = 1.5 * g
+    furthest_point_achieved = 0
+    total_time = 0
+
+    number_of_points = len(lap)
+    
+    for i in range(number_of_points):
+        available_percentage = 1
+
+        previous_point, previous_speed = lap[i - 1]
+        current_point, current_speed = lap[i]
+        following, following_speed = lap[i + 1]
+
+        turn_radius = get_turn_radius(previous_point, current_point, following)
+
+        if turn_radius > 0:
+            centrifugal_force = (current_speed**2) / turn_radius
+            if centrifugal_force > allowed_centrifugal_force:
+                furthest_point_achieved = i
+                break
+
+            available_percentage = 1 - (centrifugal_force / allowed_centrifugal_force)
+
+        gained_speed = current_speed > following_speed
+        maximum_allowed_acceleration = 0
+        if gained_speed:
+            maximum_engine_acceleration = 0.9*g if current_speed < 15.28 else (272195/66227)*current_speed - (12250/66277)*(current_speed**2) # 15.28 m/s = 55 kph
+            maximum_allowed_acceleration = maximum_engine_acceleration * available_percentage
+        else:
+            maximum_braking_power = - 1.5 * g
+            maximum_allowed_acceleration = maximum_braking_power * available_percentage
+
+        necessary_acceleration = (following_speed**2 - current_speed**2) / (2*np.linalg.norm(previous_point - current_point))
+        if necessary_acceleration > maximum_allowed_acceleration:
+            furthest_point_achieved = i
+            break
+
+        total_time += (following_speed - current_speed) / necessary_acceleration
+
+    return total_time + ((number_of_points + 1)/(furthest_point_achieved + 1))*100000, furthest_point_achieved
 
 
 def run():

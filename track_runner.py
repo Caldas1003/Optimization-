f9b70c1b9d00202a8213e61dd77af263cbd4ec8e
NikolasRR <nkls99@gmail.com>
2024-12-20 10:28:54 -0300

    algoritmo corrigido e com geração de gráficos

--- track_runner.py
+++ track_runner.py
@@ -1,7 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import time
+from datetime import datetime
 from checkpoint_builder import CHECKPOINTS
-from diferential_evolution import evolucao_diferencial_adaptativa
+from diferential_evolution import customDifferentialEvolution
 
 
 def calculate_angle(P1, P2, P3):
@@ -18,9 +20,9 @@ def calculate_angle(P1, P2, P3):
 
 
 def get_turn_radius(P1, P2, P3):
-    p2_p3_len = np.sqrt((P3 - P2)**2)
-    p1_p3_len = np.sqrt((P3 - P1)**2)
-    p1_p2_len = np.sqrt((P2 - P1)**2)
+    p2_p3_len = np.linalg.norm(P3 - P2)
+    p1_p3_len = np.linalg.norm(P3 - P1)
+    p1_p2_len = np.linalg.norm(P2 - P1)
 
     stretch_1 = P2 - P1
     stretch_2 = P3 - P2
@@ -50,63 +52,130 @@ def total_distance(waypoints: list) -> float:
 
     return total_distance
 
-def lap_time(lap: list) -> tuple:
+def lapTime(waypoints: list, speeds: list) -> tuple:
+    number_of_points = len(waypoints)
+    
+    if number_of_points != len(speeds):
+        raise ValueError("Number of speeds must be equal to number of waypoints")
+    
     g = 9.8 # gravity (9.8 m/s²)
     allowed_centrifugal_force = 1.5 * g
-    furthest_point_achieved = 0
+    furthest_checkpoint_achieved = number_of_points - 1
     total_time = 0
-
-    number_of_points = len(lap)
+    reason_for_failure = "none"
     
     for i in range(number_of_points):
+        if i == number_of_points - 1:
+            break # the last and first point are the same, so if we reached the checkpoint 199, we are done
+
         available_percentage = 1
 
-        previous_point, previous_speed = lap[i - 1]
-        current_point, current_speed = lap[i]
-        following, following_speed = lap[i + 1]
+        previous = i - 1
+        current = i
+        following = i + 1
 
-        turn_radius = get_turn_radius(previous_point, current_point, following)
+        previous_point = CHECKPOINTS[previous][waypoints[previous]]
+        current_point = CHECKPOINTS[current][waypoints[current]]
+        following_point = CHECKPOINTS[following][waypoints[following]]
+
+        current_speed = speeds[current]
+        following_speed = speeds[following]
+
+        turn_radius = get_turn_radius(previous_point, current_point, following_point)
 
         if turn_radius > 0:
             centrifugal_force = (current_speed**2) / turn_radius
             if centrifugal_force > allowed_centrifugal_force:
-                furthest_point_achieved = i
+                furthest_checkpoint_achieved = current
+                reason_for_failure = "turn_speed"
                 break
 
             available_percentage = 1 - (centrifugal_force / allowed_centrifugal_force)
 
-        gained_speed = current_speed > following_speed
+        gained_speed = following_speed > current_speed
         maximum_allowed_acceleration = 0
         if gained_speed:
             maximum_engine_acceleration = 0.9*g if current_speed < 15.28 else (272195/66227)*current_speed - (12250/66277)*(current_speed**2) # 15.28 m/s = 55 kph
             maximum_allowed_acceleration = maximum_engine_acceleration * available_percentage
         else:
-            maximum_braking_power = - 1.5 * g
+            maximum_braking_power = 1.5 * g
             maximum_allowed_acceleration = maximum_braking_power * available_percentage
 
-        necessary_acceleration = (following_speed**2 - current_speed**2) / (2*np.linalg.norm(previous_point - current_point))
+        stretch_distance = np.linalg.norm(following_point - current_point)
+        
+        necessary_acceleration = np.abs((following_speed**2 - current_speed**2) / (2*stretch_distance))
+    
         if necessary_acceleration > maximum_allowed_acceleration:
-            furthest_point_achieved = i
+            furthest_checkpoint_achieved = current
+            reason_for_failure = "acceleration"
             break
 
-        total_time += (following_speed - current_speed) / necessary_acceleration
+        try:
+            total_time += (following_speed - current_speed) / necessary_acceleration if necessary_acceleration != 0 else stretch_distance / current_speed
+        except:
+            print(f"following_speed: {following_speed}")
+            print(f"current_speed: {current_speed}")
+            print(f"necessary_acceleration: {necessary_acceleration}")
 
-    return total_time + ((number_of_points + 1)/(furthest_point_achieved + 1))*100000, furthest_point_achieved
+    return total_time + (((number_of_points + 1)/(furthest_checkpoint_achieved + 1)) - 1)*500000, furthest_checkpoint_achieved, reason_for_failure
 
 
 def run():
     start_time = time.time()
+    F = 0.5
+    CR = 0.85
+    max_gen = 100
+    pop_size = 4000
 
-    best_params, best_fitness, standard_deviation = evolucao_diferencial_adaptativa(total_distance, np.array([(0, 99) for _ in range(100)]), max_generations=1000)
+    best_params, best_fitness, standard_deviation, data = customDifferentialEvolution(lapTime, [[0, 99], [0.278, 22.22]], max_generations=max_gen, F=F, CR=CR, pop_size=pop_size)
 
     time_elapsed = time.time() - start_time
     hours, seconds_remain = divmod(time_elapsed, 3600)
     minutes, seconds = divmod(seconds_remain, 60)
 
     print(f"Melhores parâmetros encontrados: {best_params}")
-    print(f"Menor distância total encontrada: {best_fitness}")
+    print(f"Menor tempo total encontrada: {best_fitness}")
     print(f"Desvio padrão: {standard_deviation}")
+    print(f"Ponto mais longe: {data[2][len(data[2]) - 1]}")
     print(f"Total time elapsed: {int(hours):02}:{int(minutes):02}:{int(seconds):02}")
 
+
+    generations = [i + 1 for i in range(len(data[0]))]
+    plt.figure(figsize=(18, 10))
+    plt.suptitle(f"Tempo gasto: {int(hours):02}:{int(minutes):02}:{int(seconds):02}\nDesvio padrão: {standard_deviation:.2f}\nMenor tempo: {best_fitness:.2f}\nPonto mais longe alcançado: {data[2][len(data[2]) - 1]}\n", fontsize=16)
+    plt.subplots_adjust(wspace=0.8, hspace=1.2)
+
+    plt.subplot(4, 1, 1)
+    plt.plot(generations, data[0])
+    plt.xlabel("Geração")
+    plt.ylabel("Melhor tempo de volta")
+    plt.ylim(0 , 2500)
+    plt.xlim(0, max_gen)
+
+    plt.subplot(4, 1, 2)
+    plt.plot(generations, data[1])
+    plt.xlabel("Geração")
+    plt.ylabel("Desvio Padrão")
+    plt.ylim(0, 10)
+    plt.xlim(0, max_gen)
+
+    plt.subplot(4, 1, 3)
+    plt.plot(generations, data[2])
+    plt.xlabel("Geração")
+    plt.ylabel("Ponto mais longe alcançado")
+    plt.xlim(0, max_gen)
+
+    # categories, count = np.unique(data[4], return_counts=True)
+    # print(categories)
+    # print(count)
+    # plt.subplot(4, 1, 4)
+    # plt.bar(categories, count)
+    # plt.xlabel("Motivo da falha")
+    # plt.ylabel("Quantidade")
+    # plt.xticks(np.arange(len(categories)), categories)
+    # plt.subplot(2, 1, 4).plot(generations, data[3], ylabel="Desvio Padrão", xlabel="Geração")
+
+    timestamp = datetime.now().strftime("%H%M %d-%m-%Y")
+    plt.savefig(f"charts/{timestamp} pop_size={pop_size} max_gen={max_gen} F={F} CR={CR}.png")
 
 run()

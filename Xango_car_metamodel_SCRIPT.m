d4e33d2f15d43c1c4fe2a617f6894fc4203b9504
Jao-Franca <159197482+Jao-Franca@users.noreply.github.com>
2025-03-17 15:57:24 -0300

    Update Xango_car_metamodel_SCRIPT.m

--- Xango_car_metamodel_SCRIPT.m
+++ Xango_car_metamodel_SCRIPT.m
@@ -1,79 +1,84 @@
 % function [] = Xango_car_metamodel_SCRIPT()
 %% Run this script befor running the Xango_car_metamodel project
 
-%% InicializaÁ„o
+%% Inicializa√ß√£o
 clc; clear all; close all;
 
 %% Masses
-Mc = 300;       % massa do carro [kg]
-Mst = 120;       % massa suspensa traseira [kg]
-Msd = 100;       % massa suspensa dianteira [kg]
+Mc = 280;       % massa do carro [kg]
+Mst = 25;       % massa suspensa traseira [kg]
+Msd = 20;       % massa suspensa dianteira [kg]
 
 %% Rigidez
-Kte = 5e6;      % rigidez da suspens„o traseira esquerda [N/m]
-Ktd = 5e6;      % rigidez da suspens„o traseira direita [N/m]
-Kde = 3e6;      % rigidez da suspens„o dianteira esquerda [N/m]
-Kdd = 3e6;      % rigidez da suspens„o dianteira direita [N/m]
-Kpte = 2e6;     % rigidez do pneu traseiro esquerdo [N/m]
-Kptd = 2e6;     % rigidez do pneu traseiro direito [N/m]
-Kpde = 2e6;     % rigidez do pneu dianteiro esquerdo [N/m]
-Kpdd = 2e6;     % rigidez do pneu dianteiro direito [N/m]
+Kte = 5e5;      % rigidez da suspens√£o traseira esquerda [N/m]
+Ktd = 5e5;      % rigidez da suspens√£o traseira direita [N/m]
+Kde = 5e5;      % rigidez da suspens√£o dianteira esquerda [N/m]
+Kdd = 5e5;      % rigidez da suspens√£o dianteira direita [N/m]
+Kpte = 2e4;     % rigidez do pneu traseiro esquerdo [N/m]
+Kptd = 2e4;     % rigidez do pneu traseiro direito [N/m]
+Kpde = 2e4;     % rigidez do pneu dianteiro esquerdo [N/m]
+Kpdd = 2e4;     % rigidez do pneu dianteiro direito [N/m]
+
 Kf = 3.01e7;    % rigidez flexional do chassis [N/m]
 Kt = 1.6e7;     % rigidez torcional do chassis [N/m]
 
 %% Amortecedores
-Cte = 5e3;      % amortecimento da suspens„o traseira esquerda [N*s/m]
-Ctd = 5e3;      % amortecimento da suspens„o traseira direita [N*s/m]
-Cde = 3e3;      % amortecimento da suspens„o dianteira esquerda [N*s/m]
-Cdd = 3e3;      % amortecimento da suspens„o dianteira direita [N*s/m]
+Cte = 5e3;      % amortecimento da suspens√£o traseira esquerda [N*s/m]
+Ctd = 5e3;      % amortecimento da suspens√£o traseira direita [N*s/m]
+Cde = 3e3;      % amortecimento da suspens√£o dianteira esquerda [N*s/m]
+Cdd = 3e3;      % amortecimento da suspens√£o dianteira direita [N*s/m]
 
-%% InÈrcias
-If = 5e6;       % inÈrcia flexional do chassis [kg*m^2]
-It = 5e6;       % inÈrcia torcional do chassis [kg*m^2]
+Cphi = 5e2;     % amortecimento estrtural de tor√ß√£o do chassis [Confia]
+Ctheta = 5e2;   % amortecimento estrtural de flex√£o do chassis [Confia]
+
+
+%% In√©rcias
+If = 5e5;       % in√©rcia flexional do chassis [m^4]
+It = 5e5;       % in√©rcia torcional do chassis [m^4]
 
 %% Geometrias
 Lt = 0.8;       % comprimento traseiro partindo do CG [m]
 Ld = 1;       % comprimento dianteiro partindo do CG [m]
 W2 = 0.45;      % metade do entre-rodas (bitola) [m]
 
-%% Din‚mica horizontal
+%% Din√¢mica horizontal
 Psid = 0;        %slip angle fixo
 
-% Vari·veis Gerais
+% Vari√°veis Gerais
 X_VL = 0; % Deslocamento Longitudinal do carro [m]
 X_VT = 0; % Deslocamento Transversal do carro [m]
 dot_X_VL = 0.001; % Velocidade Longitudinal do carro [m/s]
 dot_X_VT = 0; % Velocidade Transversal do carro [m/s]
-ddot_X_VL = 0; % AceleraÁ„o Longitudinal do carro [m/s^2]
-ddot_X_VT = 0; % AceleraÁ„o Transversal do carro [m/s^2]
+ddot_X_VL = 0; % Acelera√ß√£o Longitudinal do carro [m/s^2]
+ddot_X_VT = 0; % Acelera√ß√£o Transversal do carro [m/s^2]
 
 mu = 1.45; % Coeficiente de atrito [adimensional]
 
 dot_omega_d = 0; % Velocidade Angular do pneu dianteiro esquerdo [rad/s]
 dot_omega_t = 0; % Velocidade Angular do pneu traseiro esquerdo [rad/s]
-ddot_omega_d = 0; % AceleraÁ„o Angular do pneu dianteiro esquerdo [rad/s^2]
-ddot_omega_t = 0; % AceleraÁ„o Angular do pneu traseiro esquerdo [rad/s^2]
+ddot_omega_d = 0; % Acelera√ß√£o Angular do pneu dianteiro esquerdo [rad/s^2]
+ddot_omega_t = 0; % Acelera√ß√£o Angular do pneu traseiro esquerdo [rad/s^2]
 
-alpha = 5 * pi / 180; % Convers„o de graus para radianos
+alpha = 5 * pi / 180; % Convers√£o de graus para radianos
 a = 1.2; % Constante de Ackermann [adimensional]
-gamma = 9; % ¬ngulo de slip Traseiro [graus]
+gamma = 9; % √Çngulo de slip Traseiro [graus]
 lambda = 9; % Slip Ratio [adimensional]
 
-% ¬ngulos de C‚mber
-C_alpha_e = 0.5; % ¬ngulo de C‚mber Esquerdo [graus]
-C_alpha_d = 0.5; % ¬ngulo de C‚mber Direito [graus]
-C_gamma = 0; % ¬ngulo de C‚mber Traseiro [rad]
+% √Çngulos de C√¢mber
+C_alpha_e = 0.5; % √Çngulo de C√¢mber Esquerdo [graus]
+C_alpha_d = 0.5; % √Çngulo de C√¢mber Direito [graus]
+C_gamma = 0; % √Çngulo de C√¢mber Traseiro [rad]
 
-% ForÁas Laterais associadas ao C‚mber
-C_tde = 0; % ForÁa Lateral Dianteira Esquerda [N]
-C_tdd = 0; % ForÁa Lateral Dianteira Direita [N]
-C_tte = 0; % ForÁa Lateral Traseira Esquerda [N]
-C_ttd = 0; % ForÁa Lateral Traseira Direita [N]
+% For√ßas Laterais associadas ao C√¢mber
+C_tde = 0; % For√ßa Lateral Dianteira Esquerda [N]
+C_tdd = 0; % For√ßa Lateral Dianteira Direita [N]
+C_tte = 0; % For√ßa Lateral Traseira Esquerda [N]
+C_ttd = 0; % For√ßa Lateral Traseira Direita [N]
 
 Rdp = 0.3; % Raio do pneu [m]
 
-% Massas, InÈrcias e Cargas
-I_p = 1.0; % InÈrcia do pneu [kg.m^2]
+% Massas, In√©rcias e Cargas
+I_p = 1.0; % In√©rcia do pneu [kg.m^2]
 F_zde = Mc*9.81/4; % Carga Vertical Dianteira Esquerda [N]
 F_zdd = Mc*9.81/4; % Carga Vertical Dianteira Direita [N]
 F_zte = Mc*9.81/4; % Carga Vertical Traseira Esquerda [N]
@@ -81,10 +86,10 @@ F_ztd = Mc*9.81/4; % Carga Vertical Traseira Direita [N]
 
 % Entradas
 tau_F = 500; % Torque de frenagem [N.m]
-tau_M = 1000; % Torque Mec‚nico (PWT) [N.m]
+tau_M = 1000; % Torque Mec√¢nico (PWT) [N.m]
 
 
-% Par‚metros de Pacejka
+% Par√¢metros de Pacejka
 E = 0.3336564873588197; 
 C_y = 1.627;
 C_x = 1.0;
@@ -92,31 +97,31 @@ C_z = 1.0;
 c1 = 931.405;
 c2 = 366.493;
 
-% Par‚metros adicionais (exemplos)
-Ld = 1.2; % Dist‚ncia do CG para dianteiro [m]
-Lt = 1.2; % Dist‚ncia do CG para traseiro [m]
+% Par√¢metros adicionais (exemplos)
+Ld = 1.2; % Dist√¢ncia do CG para dianteiro [m]
+Lt = 1.2; % Dist√¢ncia do CG para traseiro [m]
 W = 0.9; % Largura do carro [m]
 H_cg = 0.3; % Altura do centro de gravidade [m]
-I_psi = 1e7; % InÈrcia em Yaw [kg.m^2]
-I_rho = 1e7; % InÈrcia em Rolagem [kg.m^2]
-I_beta = 1e7; % InÈrcia em Pitch [kg.m^2]
-M_total = 300; % Massa total do veÌculo [kg]
+I_psi = 1e7; % In√©rcia em Yaw [kg.m^2]
+I_rho = 1e7; % In√©rcia em Rolagem [kg.m^2]
+I_beta = 1e7; % In√©rcia em Pitch [kg.m^2]
+M_total = 300; % Massa total do ve√≠culo [kg]
 
-%% 2. C·lculo dos Par‚metros Intermedi·rios
+%% 2. C√°lculo dos Par√¢metros Intermedi√°rios
 
-% C_s em funÁ„o da carga
+% C_s em fun√ß√£o da carga
 C_s_de = c1 * sin(2 * atan(F_zde / c2));
 C_s_dd = c1 * sin(2 * atan(F_zdd / c2));
 C_s_te = c1 * sin(2 * atan(F_zte / c2));
 C_s_td = c1 * sin(2 * atan(F_ztd / c2));
 
-% D em funÁ„o da carga
+% D em fun√ß√£o da carga
 D_de = mu * F_zde;
 D_dd = mu * F_zdd;
 D_te = mu * F_zte;
 D_td = mu * F_ztd;
 
-% B_x, B_y e B_z em funÁ„o da carga
+% B_x, B_y e B_z em fun√ß√£o da carga
 B_z_de = C_s_de / (C_z * D_de);
 B_x_de = C_s_de / (C_x * D_de);
 B_y_de = C_s_de / (C_y * D_de);
@@ -133,26 +138,26 @@ B_z_td = C_s_td / (C_z * D_td);
 B_x_td = C_s_td / (C_x * D_td);
 B_y_td = C_s_td / (C_y * D_td);
 
-%% 3. C·lculo das ForÁas e Momentos nas Quatro Rodas
+%% 3. C√°lculo das For√ßas e Momentos nas Quatro Rodas
 
-% C·lculo das aceleraÁıes angulares dos pneus
+% C√°lculo das acelera√ß√µes angulares dos pneus
 ddot_omega_d = -tau_F / I_p;
 ddot_omega_t = (tau_M - tau_F) / I_p;
 
-% C·lculo do ‚ngulo de Ackermann
+% C√°lculo do √¢ngulo de Ackermann
 alpha_a = alpha * a;
 
-% C·lculo do Slip Ratio
+% C√°lculo do Slip Ratio
 lambda_d = (Rdp * dot_omega_d / dot_X_VL) - 1;
 lambda_t = (Rdp * dot_omega_t / dot_X_VL) - 1;
 
-% ForÁas Laterais
+% For√ßas Laterais
 F_yde = D_de * sin(C_y * atan(B_y_de * alpha - E * (B_y_de * alpha - atan(B_y_de * alpha)))) + C_tde;
 F_ydd = D_dd * sin(C_y * atan(B_y_dd * alpha_a - E * (B_y_dd * alpha_a - atan(B_y_dd * alpha_a)))) + C_tdd;
 F_yte = D_te * sin(C_y * atan(B_y_te * gamma - E * (B_y_te * gamma - atan(B_y_te * gamma)))) + C_tte;
 F_ytd = D_td * sin(C_y * atan(B_y_td * gamma - E * (B_y_td * gamma - atan(B_y_td * gamma)))) + C_ttd;
 
-% ForÁas Longitudinais
+% For√ßas Longitudinais
 F_xde = D_de * sin(C_x * atan(9 * B_x_de * lambda_de - E * (B_x_de * lambda_de - atan(B_x_de * lambda_de))));
 F_xdd = D_dd * sin(C_x * atan(9 * B_x_dd * lambda_dd - E * (B_x_dd * lambda_dd - atan(B_x_dd * lambda_dd))));
 F_xte = D_te * sin(C_x * atan(9 * B_x_te * lambda_te - E * (B_x_te * lambda_te - atan(B_x_te * lambda_te))));
@@ -170,7 +175,7 @@ C_tdd_calc = (D_dd / 2) * sin(C_y * atan(B_y_dd * C_alpha_dd));
 C_tte_calc = (D_te / 2) * sin(C_y * atan(B_y_te * C_gamma));
 C_ttd_calc = (D_td / 2) * sin(C_y * atan(B_y_td * C_gamma));
 
-%% 4. C·lculo dos Momentos Totais
+%% 4. C√°lculo dos Momentos Totais
 
 % Momento de Yaw (M_psi)
 M_psi = (F_yde + F_ydd) * Ld - (F_yte + F_ytd) * Lt + (F_xde - F_xdd + F_xte - F_xtd) * W;
@@ -181,41 +186,27 @@ M_rho = (F_yde + F_ydd + F_yte + F_ytd) * H_cg;
 % Momento de Pitch (M_beta)
 M_beta = (F_xde + F_xdd + F_xte + F_xtd) * H_cg;
 
-%% 5. C·lculo das AceleraÁıes Angulares
+%% 5. C√°lculo das Acelera√ß√µes Angulares
 
-ddot_psi = M_psi / I_psi; % AceleraÁ„o Angular de Yaw [rad/s^2]
-ddot_rho = M_rho / I_rho; % AceleraÁ„o Angular de Rolagem [rad/s^2]
-ddot_beta = M_beta / I_beta; % AceleraÁ„o Angular de Pitch [rad/s^2]
+ddot_psi = M_psi / I_psi; % Acelera√ß√£o Angular de Yaw [rad/s^2]
+ddot_rho = M_rho / I_rho; % Acelera√ß√£o Angular de Rolagem [rad/s^2]
+ddot_beta = M_beta / I_beta; % Acelera√ß√£o Angular de Pitch [rad/s^2]
 
-%% 6. C·lculo das AceleraÁıes Radiais
+%% 6. C√°lculo das Acelera√ß√µes Radiais
 
-% AceleraÁ„o Radial Longitudinal
+% Acelera√ß√£o Radial Longitudinal
 sum_Fx = F_xde + F_xdd + F_xte + F_xtd;
 ddot_X_VL = sum_Fx / M_total;
 
-% AceleraÁ„o Radial Transversal
+% Acelera√ß√£o Radial Transversal
 sum_Fy = F_yde + F_ydd + F_yte + F_ytd;
 ddot_X_VT = sum_Fy / M_total;
 
-%% 7. ExibiÁ„o dos Resultados
+%% 7. Exibi√ß√£o dos Resultados
 
 disp('===== Resultados =====');
-fprintf('AceleraÁ„o Longitudinal (ddot_X_VL): %.4f m/s^2\n', ddot_X_VL);
-fprintf('AceleraÁ„o Transversal (ddot_X_VT): %.4f m/s^2\n', ddot_X_VT);
-fprintf('AceleraÁ„o Angular de Yaw (ddot_psi): %.4f rad/s^2\n', ddot_psi);
-fprintf('AceleraÁ„o Angular de Rolagem (ddot_rho): %.4f rad/s^2\n', ddot_rho);
-fprintf('AceleraÁ„o Angular de Pitch (ddot_beta): %.4f rad/s^2\n', ddot_beta);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+fprintf('Acelera√ß√£o Longitudinal (ddot_X_VL): %.4f m/s^2\n', ddot_X_VL);
+fprintf('Acelera√ß√£o Transversal (ddot_X_VT): %.4f m/s^2\n', ddot_X_VT);
+fprintf('Acelera√ß√£o Angular de Yaw (ddot_psi): %.4f rad/s^2\n', ddot_psi);
+fprintf('Acelera√ß√£o Angular de Rolagem (ddot_rho): %.4f rad/s^2\n', ddot_rho);
+fprintf('Acelera√ß√£o Angular de Pitch (ddot_beta): %.4f rad/s^2\n', ddot_beta);
